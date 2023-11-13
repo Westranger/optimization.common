fcb3bc9139916f8aef02e3531e999151e3ade06c
Marius <marius.hoffmann@westranger.de>
2023-11-13 07:19:51 +0100

    the optimiza paramter tool now fully utilizes the CPU when properly configured

--- src/test/java/test/de/westranger/optimization/common/algorithm/tools/OptimizeParameter.java
+++ src/test/java/test/de/westranger/optimization/common/algorithm/tools/OptimizeParameter.java
@@ -38,11 +38,11 @@ public final class OptimizeParameter {
 
     input.put("tMax", List.of(0.0));
     input.put("initialAcceptanceRatio", Arrays.asList(0.9, 0.8, 0.7));
-    input.put("gamma", Arrays.asList(0.7, 0.8, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99, 0.999));
+    input.put("gamma", Arrays.asList(0.6, 0.7, 0.8, 0.9, 0.95, 0.96, 0.97, 0.98, 0.99, 0.999));
     input.put("tMin", Arrays.asList(0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0));
     input.put("omegaMax",
         Arrays.asList(100.0, 250.0, 500.0, 1000.0, 2500.0, 5000.0, 10000.0, 25000.0, 50000.0,
-            100000.0));
+            100000.0, 250000.0));
     input.put("maxImprovementPerTemperature",
         Arrays.asList(2.0, 5.0, 10.0, 25.0, 50.0, 100.0, 250.0, 500.0));
 
@@ -59,33 +59,43 @@ public final class OptimizeParameter {
         "starting parameter optimization, there are " + combinationIterator.getNumCombinations() +
             " parameter combinations which will be evaluated");
 
-    while (combinationIterator.hasNext()) {
-      final ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
-      final CompletionService<Map<String, Double>> completionService =
-          new ExecutorCompletionService<>(executorService);
+    int activeTaskCounter = 0;
 
-      int totalTasksSubmitted = 0;
-      for (int i = 0; i < batchSize && combinationIterator.hasNext(); i++) {
+    final ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+    final CompletionService<Map<String, Double>> completionService =
+        new ExecutorCompletionService<>(executorService);
+
+    int totalTasksSubmitted = 0;
+    while (combinationIterator.hasNext()) {
+
+      while (activeTaskCounter < threadPoolSize && combinationIterator.hasNext()) {
+//      for (int i = 0; i < batchSize && combinationIterator.hasNext(); i++) {
         final Map<String, Double> combination = combinationIterator.next();
         final Callable<Map<String, Double>> task = new TSPCallable(problem, combination, numTries);
         completionService.submit(task);
         totalTasksSubmitted++;
+        activeTaskCounter++;
       }
 
-      pt.nextRound(combinationIterator.getCombinationCounter());
-      final long current = System.currentTimeMillis();
-      System.out.println(
-          "[" + sdf.format(new Date(current)) + "] submitted next batch of " + batchSize +
-              " jobs, total progress: " + df.format(pt.getProgressPercentage()) +
-              " % estimated finish time " + sdf.format(new Date(pt.getEstimatedCompletionDate())) +
-              " avg time for job " + df.format(pt.getAverageDelta()) + " ms");
+      if (totalTasksSubmitted % batchSize == 0) {
+        pt.nextRound(combinationIterator.getCombinationCounter());
+        final long current = System.currentTimeMillis();
 
-      executorService.shutdown();
+        System.out.println(
+            "[" + sdf.format(new Date(current)) + "] submitted next batch of " + batchSize +
+                " jobs, total progress: " + df.format(pt.getProgressPercentage()) +
+                " % estimated finish time " +
+                sdf.format(new Date(pt.getEstimatedCompletionDate())) +
+                " avg time for job " + df.format(pt.getAverageDelta()) + " ms");
+      }
 
-      for (int i = 0; i < totalTasksSubmitted; ++i) {
+      if (activeTaskCounter > 0)
+      //for (int i = 0; i < totalTasksSubmitted; ++i) {
+      {
         try {
           Future<Map<String, Double>> future = completionService.take();
           Map<String, Double> result = future.get();
+          activeTaskCounter--;
           if (bestScore > result.get("score") ||
               (Math.abs(bestScore - result.get("score")) < 1e-3 && minIter > result.get("iter"))) {
             System.out.println('\t' + result.toString());
@@ -97,5 +107,7 @@ public final class OptimizeParameter {
         }
       }
     }
+    executorService.shutdown();
   }
 }
+
